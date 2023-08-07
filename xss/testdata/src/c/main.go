d580d45c75fb6b8749bbe1b27300618fde5c365d
Kent 'picat' Gruber <kent.picat.gruber@gmail.com>
2023-08-07 22:09:00 +0000

    Expand `TestC`

--- xss/testdata/src/c/main.go
+++ xss/testdata/src/c/main.go
@@ -17,8 +17,37 @@ func mirror(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func mirror2(w http.ResponseWriter, r *http.Request) {
+	_, err := io.WriteString(w, r.URL.Query().Get("q")) // want "potential XSS"
+	if err != nil {
+		panic(err)
+	}
+}
+
+func mirror3(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte(r.URL.Query().Get("q"))) // want "potential XSS"
+	if err != nil {
+		panic(err)
+	}
+}
+
+func mirror4(w http.ResponseWriter, r *http.Request) {
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = w.Write(b) // want "potential XSS"
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
-	http.HandleFunc("/", mirror)
+	http.HandleFunc("/1", mirror)
+	http.HandleFunc("/2", mirror2)
+	http.HandleFunc("/3", mirror3)
+	http.HandleFunc("/4", mirror4)
 
 	http.ListenAndServe(":8080", nil)
 }
