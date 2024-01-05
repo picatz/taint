1d4aa7f3a8d299c59df39117cac2c8110602e56f
Kent 'picat' Gruber <kent@hashicorp.com>
2024-01-05 18:09:12 -0500

    Use cluster subgraphs to scope package functions
    This doesn't work for all Graphviz layout engines, but it's nice when it does.

--- callgraphutil/dot.go
+++ callgraphutil/dot.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"strings"
 
 	"golang.org/x/tools/go/callgraph"
 )
@@ -22,17 +23,58 @@ func WriteDOT(w io.Writer, g *callgraph.Graph) error {
 
 	edges := []*callgraph.Edge{}
 
+	nodesByPkg := map[string][]*callgraph.Node{}
+
+	addPkgNode := func(n *callgraph.Node) {
+		// TODO: fix this so there's not so many "shared" functions?
+		//
+		// It is a bit of a hack, but it works for now.
+		var pkgPath string
+		if n.Func.Pkg != nil {
+			pkgPath = n.Func.Pkg.Pkg.Path()
+		} else {
+			pkgPath = "shared"
+		}
+
+		// Check if the package already exists.
+		if _, ok := nodesByPkg[pkgPath]; !ok {
+			// If not, create it.
+			nodesByPkg[pkgPath] = []*callgraph.Node{}
+		}
+		nodesByPkg[pkgPath] = append(nodesByPkg[pkgPath], n)
+	}
+
 	// Check if root node exists, if so, write it.
 	if g.Root != nil {
 		b.WriteString(fmt.Sprintf("\troot = %d;\n", g.Root.ID))
 	}
 
-	// Write nodes.
+	// Process nodes and edges.
 	for _, n := range g.Nodes {
-		b.WriteString(fmt.Sprintf("\t%d [label=%q];\n", n.ID, n.Func))
+		// Add node to map of nodes by package.
+		addPkgNode(n)
 
 		// Add edges
 		edges = append(edges, n.Out...)
+	}
+
+	// Write nodes by package.
+	for pkg, nodes := range nodesByPkg {
+		// Make the pkg name sugraph cluster friendly (remove dots, dashes, and slashes).
+		clusterName := strings.Replace(pkg, ".", "_", -1)
+		clusterName = strings.Replace(clusterName, "/", "_", -1)
+		clusterName = strings.Replace(clusterName, "-", "_", -1)
+
+		// NOTE: even if we're using a subgraph cluster, it may not be
+		// respected by all Graphviz layout engines. For example, the
+		// "dot" engine will respect the cluster, but the "sfdp" engine
+		// will not.
+		b.WriteString(fmt.Sprintf("\tsubgraph cluster_%s {\n", clusterName))
+		b.WriteString(fmt.Sprintf("\t\tlabel=%q;\n", pkg))
+		for _, n := range nodes {
+			b.WriteString(fmt.Sprintf("\t\t%d [label=%q];\n", n.ID, n.Func))
+		}
+		b.WriteString("\t}\n")
 	}
 
 	// Write edges.
